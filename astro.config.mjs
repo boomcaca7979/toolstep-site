fb9dfdc5d17c9d59a24d1150f6665ee6c5b136ea
QiuYue <boomcaca666@gmail.com>
2026-05-29 01:18:56 +0800

    fix google verification code

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://toolwise-review.surge.sh',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
